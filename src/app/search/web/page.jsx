8b641b779bb6fbab4bfde33de1ccb53952fefe20
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-05 22:21:56 +0000

    add image results

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default async function WebSearchPage({searchParams}) {
 
-    const searchTerm = searchParams.searchTerm;
+    const { searchTerm } = await searchParams;
 
     const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}`);
 
@@ -28,8 +28,8 @@ export default async function WebSearchPage({searchParams}) {
 
   return (
     <div>
-    
-        {results && <WebSearchResults results={data}/>}
+        
+        {results && <WebSearchResults results={data}/>} 
     </div>
   )
 }
